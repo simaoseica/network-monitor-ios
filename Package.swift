421f7e2fd63432a6ff5f4963a9c668d03f5e0af3
Simão Seiça <simaoseica@gmail.com>
2024-09-16 12:31:42 +0100

    Library as dynamic

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "FNMNetworkMonitor",
+            type: .dynamic,
             targets: ["FNMNetworkMonitor"]),
     ],
     dependencies: [],
